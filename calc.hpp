4e01c432c5e81f6af6ef8c4709a5639386491d45
Karen <karenvardumyan@gmail.com>
2018-05-16 17:29:31 +0400

    mult

--- calc.hpp
+++ calc.hpp
@@ -5,14 +5,11 @@
 
 int sum();
 int diff();
-<<<<<<< HEAD
 int div();
 int mult(int,int);
 int mod();
-=======
 float div(float num1, float num2);
 int mult();
->>>>>>> e8d02389637eee924352b37294095af9f5e22b51
 float sqrt();
 int mod(int, int);
 int ln();
