0f25b5776d03df44a8ff4bf98d79b433d2434241
Arman_Anahit <avdalyan1999@gmail.com>
2018-05-16 17:07:34 +0400

    Changed main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 
 int main() {
     int temp = 0;
+    int num1 = 0;
+    int num2 = 0;
     do{
-    std::cout<<"For sumuation enter 1\nFor subtraction enter 2\nFor division enter3 \n For multiplication enter 4 \n For percentage enter5 \n For root enter 6\n For exponent enter 7\n For exit enter 0"<<\n;
-    std::cin>>temp;
+	std::cout<<"For sumuation enter 1\nFor subtraction enter 2\nFor division enter3 \n For multiplication enter 4 \n For mod enter5 \n For root enter 6\n For exponent enter 7\n For exit enter 0"<<\n;
+    	std::cin>>temp;
 
-    switch(temp) {
-    //ameqd dzer gorcoxutyan hamar greq dzer case ev function@
-    }
+    	switch(temp) {
+    	    case 5:
+		std :: cout << "Input numbers(like this 4 8)";
+		std :: cin >> num1 >> num2;
+		std :: cout << mod(num1, num2);
+		break;
+    	}
     } while (temp != 0);
     
     return 0;
