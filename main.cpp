b9ceb7d9e0bb8a22de7dcc567bf711b8a526de2b
BMariam <mariambaghdasaryan77@gmail.com>
2018-05-16 17:26:27 +0400

    s

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
     case 1: 
         int num1;
         int num2;
+        std :: cin >> num1 >> num2;
         std :: cout << sum(num1, num2) << "\n";
     }
     } while (temp != 0);
