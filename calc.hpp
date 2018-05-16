8afcf1a5bf2e0979f654e7c1af70cd1f2a07c241
Arman_Anahit <avdalyan1999@gmail.com>
2018-05-16 17:53:18 +0400

    Changed some files

--- calc.hpp
+++ calc.hpp
@@ -2,14 +2,14 @@
 #define CALC_HPP
 #include <iostream>
 #include <math.h>
-int min(num1, num2);
+void min(int num1, int num2);
 int sum(int num1, int num2);
 int diff();
 float div(float num1, float num2);
 int mult();
 int mod(int, int);
 void sqrtFun(int num);
-int pow();	
-int myPow();	
+int pow();
+int myPow();
 
 #endif
